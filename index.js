2b08a321834c072d5024cbf1b07b978dc8edf52e
Abhishek Sharma <abhisharma.hp@gmail.com>
2020-12-27 23:43:32 +0530

    make it better

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    }
-  })
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 // different values of room creates different rooms
-const room = 'afasds'
+const room = "afasds";
 
-app.get('/' , (req,res) => {
-    res.send("I am on!!!")
-})
+app.get("/", (req, res) => {
+  res.send("I am on!!!");
+});
 
-app.get('/name' , (req,res)=>{
-    res.send("Abhishek Sharma")
-})
+app.get("/name", (req, res) => {
+  res.send("Abhishek Sharma");
+});
 
 // server
-io.on('connection',socket=>{
-    // new socket requests to join room
-    socket.on('join-room' ,userId=>{
-        console.log("userConneted with id: ",userId) // debuging
-        // server joins client to room 
-        socket.join(room)
-        // server sends a message to all in room except current user that a new user is connected
-        socket.to(room).broadcast.emit('user-connected',userId)
-    })
+io.on("connection", socket => {
+  // new socket requests to join room
+  socket.on("join-room", userId => {
+    // server joins client to room
+    socket.join(room);
+    // server sends a message to all in room except current user that a new user is connected
+    socket.to(room).broadcast.emit("user-connected", userId);
+    // tell everyone a user has left
+    socket.on("disconnect", () => {
+      socket.to(room).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
 
-})
-
-const PORT = process.env.PORT || 8000
-server.listen(PORT,()=>console.log( `Server Running at port: ${PORT}` ))
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`Server Running at port: ${PORT}`));
